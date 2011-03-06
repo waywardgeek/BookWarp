0bf75f0665aeefcb27cd3b49276dd8971ac46429
Bill Cox <waywardgeek@gmail.com>
2011-03-06 08:58:53 -0500

    Added onClick callbacks to all buttons

--- src/org/vinux/project/bookwarp/BookWarpUI.java
+++ src/org/vinux/project/bookwarp/BookWarpUI.java
@@ -2,6 +2,7 @@ package org.vinux.project.bookwarp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BookWarpUI extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,27 @@ public class BookWarpUI extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void switchToBookshelf(View v) {
+    }
+    
+    public void switchToContents(View v) {
+    }
+
+    public void previousChapter(View v) {
+    }
+
+    
+    public void backward(View v) {
+    }
+
+    public void playPause(View v) {
+    }
+
+    public void forward(View v) {
+    }
+
+    public void nextChapter(View v) {
     }
 }
