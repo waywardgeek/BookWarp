a60fd849f0ebcfb46b2d777346110a8d7d8c33a3
Bill Cox <waywardgeek@gmail.com>
2011-03-06 07:07:03 -0500

    Built a layout, but not any code for it

--- gen/org/vinux/project/bookwarp/R.java
+++ gen/org/vinux/project/bookwarp/R.java
@@ -13,11 +13,46 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int LinearLayout01=0x7f050007;
+        public static final int backward=0x7f050010;
+        public static final int bookshelf=0x7f050003;
+        public static final int chapter=0x7f050009;
+        public static final int chapter_slider=0x7f05000b;
+        public static final int contents=0x7f050004;
+        public static final int cover_art=0x7f050001;
+        public static final int forward=0x7f050012;
+        public static final int linearLayout1=0x7f05000e;
+        public static final int linearLayout3=0x7f050000;
+        public static final int linearLayout4=0x7f050002;
+        public static final int next_chapter=0x7f050013;
+        public static final int play=0x7f050011;
+        public static final int position=0x7f050008;
+        public static final int position_slider=0x7f050006;
+        public static final int prev_chapter=0x7f05000f;
+        public static final int speed=0x7f05000c;
+        public static final int speed_slider=0x7f05000d;
+        public static final int textView3=0x7f05000a;
+        public static final int title=0x7f050005;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int backward=0x7f04000c;
+        public static final int bookshelf=0x7f040006;
+        public static final int chapter=0x7f040008;
+        public static final int contents=0x7f040007;
+        public static final int current_time=0x7f040004;
+        public static final int duration=0x7f040005;
+        public static final int forward=0x7f04000d;
+        public static final int next_chapter=0x7f04000b;
+        public static final int now_playing=0x7f040000;
+        public static final int play=0x7f04000e;
+        public static final int prev_chapter=0x7f04000a;
+        public static final int speed=0x7f040009;
+        public static final int speed_control=0x7f040002;
+        public static final int title=0x7f040003;
     }
 }
